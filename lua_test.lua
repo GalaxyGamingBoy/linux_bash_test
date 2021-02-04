10f424938bf1da4f3e70cc6b056d047b71985902
GalaxyGamingBoy <53054461+GalaxyGamingBoy@users.noreply.github.com>
2021-02-04 18:20:16 +0200

    Add files via upload

---
+++ lua_test.lua
@@ -0,0 +1,5 @@
+print("Lua Test")
+print("Hello World")
+i = 1 + 2
+print("1 + 2 is: ")
+print(i)
---
+++ python_test.py
@@ -0,0 +1,15 @@
+print("Hello World!")
+print("This is python")
+i = 5
+ii = 2
+q = i + ii
+qq = i - ii
+qqq = i * ii
+qqqq = i / ii
+print(i)
+print(ii)
+print(q)
+print(qq)
+print(qqq)
+print(qqqq)
+
